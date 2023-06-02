53e09b82b1dbb37484268c3c2856fd733a2bceb7
vjp2109 <vjp2109@columbia.edu>
2023-06-02 14:59:24 -0400

    psuedoCode binary search

---
+++ BinarySearch/psuedoPractice.js
@@ -0,0 +1,50 @@
+function binarySearch(arr, n) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+    let currElem = arr[middle];
+
+    if (currElem === n) {
+      return arr.indexOf(currElem);
+    } else if (currElem < n) {
+      left = ++middle;
+    } else if (currElem > n) {
+      right = ++middle;
+    }
+  }
+  return -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5], 2));
+console.log(binarySearch([1, 2, 3, 4, 5], 3));
+console.log(binarySearch([1, 2, 3, 4, 5], 5));
+console.log(binarySearch([1, 2, 3, 4, 5], 6));
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    10
+  )
+);
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    95
+  )
+);
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    100
+  )
+);
