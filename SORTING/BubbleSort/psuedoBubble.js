007d6a2b98e3793efa8149089ef83784abd01eef
vjp2109 <vjp2109@columbia.edu>
2023-06-04 17:39:55 -0400

    bubbleSort

---
+++ SORTING/BubbleSort/psuedoBubble.js
@@ -0,0 +1,49 @@
+// Naive way
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// A bit better
+
+// function bubbleSort(arr) {
+//   for (let i = arr.length; i > 0; i--) {
+//     // This line is so that after one pass through, we do not keep comparing to the end of the array,
+//     for (let j = 0; j < i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// Best Optimized way with noSwaps if the array is NEARLY SORTED
+function bubbleSort(arr) {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
+
+console.log(bubbleSort([5, 10, 0, 15, -5]));
